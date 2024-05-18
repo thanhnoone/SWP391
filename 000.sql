8f3d941cf673b982efac31ffb0809969831d2b45
datphan <tiendat.xpbp@gmail.com>
2024-05-18 10:56:39 +0700

    Database-TienDat-18.5.2024

---
+++ 000.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE swp391_db_release;
+
+USE swp391_db_release;
+
+-- 001.sql
+-- t06_role, t14_users, t09_image_avt, t13_subject, t04_center
+
+-- 002.sql
+-- t01_class, t02_schedule, t03_teacher, t05_manager
+
+-- 003.sql
+-- t07_feedback, t08_materials, t10_attendance, t11_result, t12_post
+
+-- 004.sql
+-- t15_enrollment, t16_user_center, t17_student_schedule
