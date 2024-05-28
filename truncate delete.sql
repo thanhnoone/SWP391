8bf733e4ece2c3f715ab6d36f4569d602425fb29
unknown <thanhdhde170795@gmail.com>
2024-05-28 09:47:47 +0700

    database HuuThanh 28-05-2024

---
+++ truncate delete.sql
@@ -0,0 +1,42 @@
+USE swp391_db_releasee;
+SET FOREIGN_KEY_CHECKS =0;
+
+truncate table t01_class;
+truncate table t02_schedule;
+truncate table t03_teacher;
+truncate table t04_center;
+truncate table t05_manager;
+truncate table t06_role;
+truncate table t07_feedback;
+truncate table t08_material;
+truncate table t09_image_avt;
+truncate table t10_attendance;
+truncate table t11_result;
+truncate table t12_post;
+truncate table t13_subject;
+truncate table t14_user;
+truncate table t15_enrollment;
+truncate table t16_user_center;
+truncate table t17_student_schedule;
+
+delete from t01_class;
+delete from t02_schedule;
+delete from  t03_teacher;
+delete from t04_center;
+delete from t05_manager;
+delete from t06_role;
+delete from t07_feedback;
+delete from t08_material;
+delete from t09_image_avt;
+delete from t10_attendance;
+delete from t11_result;
+delete from t12_post;
+delete from t13_subject;
+delete from t14_user;
+delete from t15_enrollment;
+delete from t16_user_center;
+delete from t17_student_schedule;
+
+
+
+SET FOREIGN_KEY_CHECKS =1;
