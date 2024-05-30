152b3903fcbff88c0b5826a58e880c06ad1b71e8
datphan <tiendat.xpbp@gmail.com>
2024-05-30 19:32:27 +0700

    Database-moinhat-30.5.2024

--- insert.sql
+++ insert.sql
@@ -19,41 +19,32 @@ INSERT INTO t14_user (C14_USER_USERNAME, C14_USER_PASSWORD, C14_ACC_STATUS, C14_
 ('parent_username_2', 'parent_password_2', 1, 20002, 'Parent 2', '123456789', '123 Street, City', '1970-01-01', 0, 'parent2@example.com', 2, NULL),
 ('teacher_username_2', 'teacher_password_2', 1, 30002, 'Teacher 2', '123456789', '123 Street, City', '1980-01-01', 1, 'teacher2@example.com', 3, NULL),
 ('manager_username_2', 'manager_password_2', 1, 40002, 'Manager 2', '123456789', '123 Street, City', '1990-01-01', 1, 'manager2@example.com', 4, NULL),
-('admin_username_2', 'admin_password_2', 1, 50002, 'Admin 2', '123456789', '123 Street, City', '1995-01-01', 1, 'admin2@example.com', 5, NULL),
-('student_username_3', 'student_password_3', 1, 10003, 'Student 3', '123456789', '123 Street, City', '2000-01-01', 1, 'student3@example.com', 1, NULL),
+('student_username_3', 'student_password_3', 1, 10004, 'Student 3', '123456789', '123 Street, City', '2000-01-01', 1, 'student4@example.com', 1, NULL),
+('student_username_4', 'student_password_4', 1, 10003, 'Student 4', '123456789', '123 Street, City', '2000-01-01', 1, 'studentABC@example.com', 1, NULL),
 ('parent_username_3', 'parent_password_3', 1, 20003, 'Parent 3', '123456789', '123 Street, City', '1970-01-01', 0, 'parent3@example.com', 2, NULL),
 ('teacher_username_3', 'teacher_password_3', 1, 30003, 'Teacher 3', '123456789', '123 Street, City', '1980-01-01', 1, 'teacher3@example.com', 3, NULL),
 ('manager_username_3', 'manager_password_3', 1, 40003, 'Manager 3', '123456789', '123 Street, City', '1990-01-01', 1, 'manager3@example.com', 4, NULL),
-('admin_username_3', 'admin_password_3', 1, 50003, 'Admin 3', '123456789', '123 Street, City', '1995-01-01', 1, 'admin3@example.com', 5, NULL),
-('student_username_4', 'student_password_4', 1, 10004, 'Student 4', '123456789', '123 Street, City', '2000-01-01', 1, 'student4@example.com', 1, NULL),
+('student_username_5', 'student_password_5', 1, 10236, 'Student 5', '123456789', '123 Street, City', '2000-01-01', 1, 'student5@example.com', 1, NULL),
+('student_username_6', 'student_password_6', 1, 10234, 'Student 6', '123456789', '123 Street, City', '2000-01-01', 1, 'student6@example.com', 1, NULL),
 ('parent_username_4', 'parent_password_4', 1, 20004, 'Parent 4', '123456789', '123 Street, City', '1970-01-01', 0, 'parent4@example.com', 2, NULL),
 ('teacher_username_4', 'teacher_password_4', 1, 30004, 'Teacher 4', '123456789', '123 Street, City', '1980-01-01', 1, 'teacher4@example.com', 3, NULL),
 ('manager_username_4', 'manager_password_4', 1, 40004, 'Manager 4', '123456789', '123 Street, City', '1990-01-01', 1, 'manager4@example.com', 4, NULL),
-('admin_username_4', 'admin_password_4', 1, 50004, 'Admin 4', '123456789', '123 Street, City', '1995-01-01', 1, 'admin4@example.com', 5, NULL);
-
-
-INSERT INTO t08_image_avt (C08_IMAGE_PATH, C08_IMAGE_TYPE, C08_USER_ID) VALUES
-('path_to_image_1', 'STUDENT', 1),
-('path_to_image_2', 'PARENT', 2),
-('path_to_image_3', 'TEACHER', 3),
-('path_to_image_4', 'MANAGER', 4),
-('path_to_image_5', 'ADMIN', 5),
-('path_to_image_6', 'STUDENT', 6),
-('path_to_image_7', 'PARENT', 7),
-('path_to_image_8', 'TEACHER', 8),
-('path_to_image_9', 'MANAGER', 9),
-('path_to_image_10', 'ADMIN', 10),
-('path_to_image_11', 'STUDENT', 11),
-('path_to_image_12', 'PARENT', 12),
-('path_to_image_13', 'TEACHER', 13),
-('path_to_image_14', 'MANAGER', 14),
-('path_to_image_15', 'ADMIN', 15),
-('path_to_image_16', 'STUDENT', 16),
-('path_to_image_17', 'PARENT', 17),
-('path_to_image_18', 'TEACHER', 18),
-('path_to_image_19', 'MANAGER', 19),
-('path_to_image_20', 'ADMIN', 20);
-
+('student_username_7', 'student_password_7', 1, 10007, 'Student 7', '123456789', '123 Street, City', '2000-01-01', 1, 'student7@example.com', 1, NULL),
+('student_username_8', 'student_password_8', 1, 10008, 'Student 8', '123456789', '123 Street, City', '2000-01-01', 1, 'student8@example.com', 1, NULL),
+('student_username_9', 'student_password_9', 1, 10009, 'Student 9', '123456789', '123 Street, City', '2000-01-01', 1, 'student9@example.com', 1, NULL),
+('student_username_10', 'student_password_10', 1, 10010, 'Student 10', '123456789', '123 Street, City', '2000-01-01', 1, 'student10@example.com', 1, NULL),
+('student_username_11', 'student_password_11', 1, 10011, 'Student 11', '123456789', '123 Street, City', '2000-01-01', 1, 'student11@example.com', 1, NULL),
+('student_username_12', 'student_password_12', 1, 10012, 'Student 12', '123456789', '123 Street, City', '2000-01-01', 1, 'student12@example.com', 1, NULL),
+('student_username_13', 'student_password_13', 1, 10013, 'Student 13', '123456789', '123 Street, City', '2000-01-01', 1, 'student13@example.com', 1, NULL),
+('student_username_14', 'student_password_14', 1, 10014, 'Student 14', '123456789', '123 Street, City', '2000-01-01', 1, 'student14@example.com', 1, NULL),
+('student_username_15', 'student_password_15', 1, 10015, 'Student 15', '123456789', '123 Street, City', '2000-01-01', 1, 'student15@example.com', 1, NULL),
+('student_username_16', 'student_password_16', 1, 10016, 'Student 16', '123456789', '123 Street, City', '2000-01-01', 1, 'student16@example.com', 1, NULL),
+('student_username_17', 'student_password_17', 1, 10017, 'Student 17', '123456789', '123 Street, City', '2000-01-01', 1, 'student17@example.com', 1, NULL),
+('student_username_18', 'student_password_18', 1, 10018, 'Student 18', '123456789', '123 Street, City', '2000-01-01', 1, 'student18@example.com', 1, NULL),
+('student_username_19', 'student_password_19', 1, 10019, 'Student 19', '123456789', '123 Street, City', '2000-01-01', 1, 'student19@example.com', 1, NULL),
+('student_username_20', 'student_password_20', 1, 10020, 'Student 20', '123456789', '123 Street, City', '2000-01-01', 1, 'student20@example.com', 1, NULL),
+('student_username_21', 'student_password_21', 1, 10021, 'Student 21', '123456789', '123 Street, City', '2000-01-01', 1, 'student21@example.com', 1, NULL);
+SELECT * FROM t14_user;
 
 INSERT INTO t13_subject (C13_SUBJECT_NAME, C13_SUBJECT_DESC) VALUES
 ('Subject 1', 'Description of Subject 1'),
@@ -172,6 +163,23 @@ VALUES
 ('Feedback 11', 16, 'teacher4@example.com'); 
 
 
+-- t09_attendance
+INSERT INTO t09_attendance (C09_ATTENDANCE_STATUS, C09_STUDENT_ID, C09_SLOT_ID) VALUES
+(1, 1, 1), (1, 6, 2), (1, 11, 3), (1, 16, 4), (1, 1, 5), (1, 6, 6), (1, 11, 7), (1, 16, 8), (1, 1, 9), (1, 6, 10), 
+(1, 11, 11), (1, 16, 12), (1, 1, 13), (1, 6, 14), (1, 11, 15), (1, 16, 16), (1, 1, 17), (1, 6, 18), (1, 11, 19),
+(1, 10, 1), (1, 10, 2), (1, 10, 3), (1, 10, 4), (1, 10, 5), (1, 10, 6), (1, 10, 7), (1, 10, 8), (1, 10, 9), (1, 10, 10), 
+(1, 10, 11), (1, 10, 12), (1, 10, 13), (1, 10, 14), (1, 10, 15), (1, 10, 16), (1, 10, 17), (1, 10, 18), (1, 10, 19), 
+(1, 15, 1), (1, 15, 2), (1, 15, 3), (1, 15, 4), (1, 15, 5), (1, 15, 6), (1, 15, 7), (1, 15, 8), (1, 15, 9), (1, 15, 10), 
+(1, 15, 11), (1, 15, 12), (1, 15, 13), (1, 15, 14), (1, 15, 15), (1, 15, 16), (1, 15, 17), (1, 15, 18), (1, 15, 19), 
+(1, 20, 1), (1, 20, 2), (1, 20, 3), (1, 20, 4), (1, 20, 5), (1, 20, 6), (1, 20, 7), (1, 20, 8), (1, 20, 9), (1, 20, 10), 
+(1, 20, 11), (1, 20, 12), (1, 20, 13), (1, 20, 14), (1, 20, 15), (1, 20, 16), (1, 20, 17), (1, 20, 18), (1, 20, 19), 
+(1, 25, 1), (1, 25, 2), (1, 25, 3), (1, 25, 4), (1, 25, 5), (1, 25, 6), (1, 25, 7), (1, 25, 8), (1, 25, 9), (1, 25, 10), 
+(1, 25, 11), (1, 25, 12), (1, 25, 13), (1, 25, 14), (1, 25, 15), (1, 25, 16), (1, 25, 17), (1, 25, 18), (1, 25, 19), 
+(1, 30, 1), (1, 30, 2), (1, 30, 3), (1, 30, 4), (1, 30, 5), (1, 30, 6), (1, 30, 7), (1, 30, 8), (1, 30, 9), (1, 30, 10), 
+(1, 30, 11), (1, 30, 12), (1, 30, 13), (1, 30, 14), (1, 30, 15), (1, 30, 16), (1, 30, 17), (1, 30, 18), (1, 30, 19);
+
+
+
 
 -- t07_material
 INSERT INTO t07_material (C07_MATERIALS_NAME, C07_TEACHER_ID, C07_SUBJECT_ID)
@@ -198,94 +206,56 @@ VALUES
 ('Materials 20', 18, 20);
 
 
-INSERT INTO t09_attendance (C09_ATTENDANCE_STATUS, C09_STUDENT_ID, C09_SLOT_ID)
-VALUES 
-(1, 1, 1),
-(1, 6, 2),
-(1, 11, 3),
-(1, 16, 4),
-(1, 1, 5),
-(1, 6, 6),
-(1, 11, 7),
-(1, 16, 8),
-(1, 1, 9),
-(1, 6, 10),
-(1, 11, 11),
-(1, 16, 12),
-(1, 1, 13),
-(1, 6, 14),
-(1, 11, 15),
-(1, 16, 16),
-(1, 1, 17),
-(1, 6, 18),
-(1, 11, 19);
+
+-- t10_result
+INSERT INTO t10_result (C10_RESULT_TYPE, C10_RESULT_VAL, C10_STUDENT_ID, C10_COURSE_ID) VALUES
+(1, 7, 1, 3), (1, 5, 6, 12), (1, 8, 11, 8), (1, 2, 16, 5), (1, 9, 1, 19), (1, 3, 6, 10),
+(2, 6, 11, 15), (1, 4, 16, 2), (2, 10, 1, 17), (1, 1, 6, 4), (1, 8, 1, 7), (1, 3, 6, 14), (1, 5, 11, 3), 
+(3, 6, 16, 18), (2, 2, 1, 10), (1, 4, 6, 5), (1, 9, 11, 19), (1, 7, 16, 12), (1, 10, 1, 2), (1, 1, 6, 15),
+(2, 7, 10, 3), (1, 5, 10, 12), (1, 8, 10, 8), (1, 2, 10, 5), (1, 9, 10, 19), 
+(2, 3, 15, 10), (3, 6, 15, 15), (2, 4, 15, 2), (1, 10, 15, 17), (1, 1, 15, 4),
+(1, 8, 20, 7), (1, 3, 20, 14), (1, 5, 20, 3), (1, 6, 20, 18), (1, 2, 20, 10),
+(2, 4, 25, 5), (2, 9, 25, 19), (2, 7, 25, 12), (1, 10, 25, 2), (1, 1, 25, 15),
+(1, 7, 30, 1), (2, 5, 30, 6), (1, 8, 30, 11), (1, 2, 30, 16), (1, 9, 30, 1),
+(1, 3, 31, 6), (1, 6, 31, 11), (1, 4, 31, 16), (1, 10, 31, 1), (1, 1, 31, 6);
 
 
-INSERT INTO t10_result (C10_RESULT_TYPE, C10_RESULT_VAL, C10_STUDENT_ID, C10_COURSE_ID)
+
+
+INSERT INTO t11_manager_admin_media (C11_TITLE, C11_CONTENT, C11_TYPE, C11_PUBLISHED_DATE, C11_SEND_TO, C11_FILE_PATH, C11_ACTOR_ID)
 VALUES
-    (1, 7, 1, 3),
-    (1, 5, 6, 12),
-    (1, 8, 11, 8),
-    (1, 2, 16, 5),
-    (1, 9, 1, 19),
-    (1, 3, 6, 10),
-    (1, 6, 11, 15),
-    (1, 4, 16, 2),
-    (1, 10, 1, 17),
-    (1, 1, 6, 4),
-    (1, 8, 1, 7),
-    (1, 3, 6, 14),
-    (1, 5, 11, 3),
-    (1, 6, 16, 18),
-    (1, 2, 1, 10),
-    (1, 4, 6, 5),
-    (1, 9, 11, 19),
-    (1, 7, 16, 12),
-    (1, 10, 1, 2),
-    (1, 1, 6, 15);
+    ('Title 1', 'Content 1', 1, CURRENT_DATE(), 'admin1@example.com', NULL, 4),
+    ('Title 2', 'Content 2', 1, CURRENT_DATE(), 'admin1@example.com', NULL, 9),
+    ('Title 3', 'Content 3', 0, CURRENT_DATE(), 'All', NULL, 5),
+    ('Title 4', 'Content 4', 1, CURRENT_DATE(), 'admin1@example.com', NULL, 14),
+    ('Title 5', 'Content 5', 0, CURRENT_DATE(), 'All', NULL, 5),
+    ('Title 6', 'Content 6', 1, CURRENT_DATE(), 'admin1@example.com', NULL, 19),
+    ('Title 7', 'Content 7', 1, CURRENT_DATE(), 'All', NULL, 5),
+    ('Title 8', 'Content 8', 0, CURRENT_DATE(), 'All', NULL, 5),
+    ('Title 9', 'Content 9', 1, CURRENT_DATE(), 'admin1@example.com', NULL, 4),
+    ('Title 10', 'Content 10', 0, CURRENT_DATE(), 'admin1@example.com', NULL, 9),
+    ('Another Title', 'Another Content', 1, CURRENT_DATE(), 'All', NULL, 5),
+    ('Another Title 2', 'Another Content 2', 1, CURRENT_DATE(), 'All', NULL, 5),
+    ('Another Title 3', 'Another Content 3', 1, CURRENT_DATE(), 'All', NULL, 5),
+    ('Another Title 4', 'Another Content 4', 1, CURRENT_DATE(), 'All', NULL, 5),
+    ('Another Title 5', 'Another Content 5', 1, CURRENT_DATE(), 'All', NULL, 5);
 
 
-
-INSERT INTO t11_manager_admin_media (C11_TITLE, C11_CONTENT, C11_TYPE, C11_PUBLISHED_DATE, C11_SEND_TO, C11_ACTOR_ID)
-VALUES
-    ('Title 1', 'Content 1', 1, CURRENT_DATE(), 'admin1@example.com', 4),
-    ('Title 2', 'Content 2', 1, CURRENT_DATE(), 'admin1@example.com', 9),
-    ('Title 3', 'Content 3', 0, CURRENT_DATE(), 'All', 11),
-    ('Title 4', 'Content 4', 1, CURRENT_DATE(), 'admin1@example.com', 14),
-    ('Title 5', 'Content 5', 0, CURRENT_DATE(), 'All', 5),
-    ('Title 6', 'Content 6', 1, CURRENT_DATE(), 'admin1@example.com', 19),
-    ('Title 7', 'Content 7', 1, CURRENT_DATE(), 'All', 15),
-    ('Title 8', 'Content 8', 0, CURRENT_DATE(), 'All', 20),
-    ('Title 9', 'Content 9', 1, CURRENT_DATE(), 'admin1@example.com', 4),
-    ('Title 10', 'Content 10', 0, CURRENT_DATE(), 'admin1@example.com', 9),
-    ('Another Title', 'Another Content', 1, CURRENT_DATE(), 'All', 11),
-    ('Another Title 2', 'Another Content 2', 0, CURRENT_DATE(), 'All', 16),
-    ('Another Title 3', 'Another Content 3', 1, CURRENT_DATE(), 'All', 5),
-    ('Another Title 4', 'Another Content 4', 0, CURRENT_DATE(), 'All', 10),
-    ('Another Title 5', 'Another Content 5', 1, CURRENT_DATE(), 'All', 15);
-
-
+-- Chèn lại dữ liệu với student id là 31, 32, 33, hoặc 34 (lựa chọn thủ công)
 INSERT INTO t15_enrollment (C15_STUDENT_ID, C15_COURSE_ID) VALUES
-    (1, 5),
-    (6, 8),
-    (11, 15),
-    (16, 3),
-    (1, 10),
-    (6, 18),
-    (11, 2),
-    (16, 7),
-    (1, 13),
-    (6, 1),
-    (11, 14),
-    (16, 6),
-    (1, 9),
-    (6, 12),
-    (11, 19),
-    (16, 4),
-    (1, 17),
-    (6, 11),
-    (11, 20),
-    (16, 16);
+(1, 5),(6, 8),(11, 15),(16, 3),(1, 10),(6, 18),(11, 2),(16, 7),(1, 13),(6, 1),
+(11, 14),(16, 6),(1, 9),(6, 12),(11, 19),(16, 4),(1, 17),(6, 11),(11, 20),(16, 16),
+(10, 1), (10, 2), (10, 3), (10, 4), (10, 5), (10, 6), (10, 7), (10, 8), (10, 9), (10, 10), (10, 11), (10, 12), (10, 13), (10, 14), (10, 15), (10, 16), (10, 17), (10, 18), (10, 19),
+(15, 1), (15, 2), (15, 3), (15, 4), (15, 5), (15, 6), (15, 7), (15, 8), (15, 9), (15, 10), (15, 11), (15, 12), (15, 13), (15, 14), (15, 15), (15, 16), (15, 17), (15, 18), (15, 19),
+(20, 1), (20, 2), (20, 3), (20, 4), (20, 5), (20, 6), (20, 7), (20, 8), (20, 9), (20, 10), (20, 11), (20, 12), (20, 13), (20, 14), (20, 15), (20, 16), (20, 17), (20, 18), (20, 19),
+(25, 1), (25, 2), (25, 3), (25, 4), (25, 5), (25, 6), (25, 7), (25, 8), (25, 9), (25, 10), (25, 11), (25, 12), (25, 13), (25, 14), (25, 15), (25, 16), (25, 17), (25, 18), (25, 19),
+(30, 1), (30, 2), (30, 3), (30, 4), (30, 5), (30, 6), (30, 7), (30, 8), (30, 9), (30, 10), (30, 11), (30, 12), (30, 13), (30, 14), (30, 15), (30, 16), (30, 17), (30, 18), (30, 19),
+(31, 1), (31, 2), (31, 3), (31, 4), (31, 5), (31, 6), (31, 7), (31, 8), (31, 9), (31, 10), (31, 11), (31, 12), (31, 13), (31, 14), (31, 15), (31, 16), (31, 17), (31, 18), (31, 19),
+(32, 1), (32, 2), (32, 3), (32, 4), (32, 5), (32, 6), (32, 7), (32, 8), (32, 9), (32, 10), (32, 11), (32, 12), (32, 13), (32, 14), (32, 15), (32, 16), (32, 17), (32, 18), (32, 19),
+(33, 1), (33, 2), (33, 3), (33, 4), (33, 5), (33, 6), (33, 7), (33, 8), (33, 9), (33, 10), (33, 11), (33, 12), (33, 13), (33, 14), (33, 15), (33, 16), (33, 17), (33, 18), (33, 19),
+(34, 1), (34, 2), (34, 3), (34, 4), (34, 5), (34, 6), (34, 7), (34, 8), (34, 9), (34, 10), (34, 11), (34, 12), (34, 13);
+
+
 
 
 INSERT INTO t16_user_center (C16_USER_ID, C16_CENTER_ID) VALUES
--- truncate delete.sql
+++ truncate delete.sql
@@ -7,7 +7,6 @@ truncate table t03_center;
 truncate table t05_role;
 truncate table t06_feedback;
 truncate table t07_material;
-truncate table t08_image_avt;
 truncate table t09_attendance;
 truncate table t10_result;
 truncate table t11_manager_admin_media;
@@ -17,24 +16,19 @@ truncate table t15_enrollment;
 truncate table t16_user_center;
 truncate table t17_student_slot;
 
-delete from t01_class;
-delete from t02_schedule;
-delete from  t03_teacher;
-delete from t04_center;
-delete from t05_manager;
-delete from t06_role;
-delete from t07_feedback;
-delete from t08_material;
-delete from t09_image_avt;
-delete from t10_attendance;
-delete from t11_result;
-delete from t12_post;
+delete from t01_course;
+delete from t02_slot;
+delete from t03_center;
+delete from t05_role;
+delete from t06_feedback;
+delete from t07_material;
+delete from t09_attendance;
+delete from t10_result;
+delete from t11_manager_admin_media;
 delete from t13_subject;
 delete from t14_user;
 delete from t15_enrollment;
 delete from t16_user_center;
-delete from t17_student_schedule;
-
-
+delete from t17_student_slot;
 
 SET FOREIGN_KEY_CHECKS = 1;
